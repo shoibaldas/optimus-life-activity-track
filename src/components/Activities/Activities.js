df385a9e8c815fcd2f94585a4a854ec53d67bb0c
shoibaldas <shoibalshantu@gmail.com>
2022-09-29 05:37:51 +0600

    activities card added and filled with json data

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,41 +1,41 @@
-// import React, { useEffect, useState } from 'react';
-// import Activity from '../Activity/Activity';
+import React, { useEffect, useState } from 'react';
+import Activity from '../Activity/Activity';
 
 
-// const Activities = () => {
-//     const [activities, setActivities] = useState([]);
+const Activities = () => {
+    const [activities, setActivities] = useState([]);
 
-//     useEffect(() => {
-//         fetch('fakedata.json')
-//             .then(res => res.json())
-//             .then(data => setActivities(data))
-//     }, [])
+    useEffect(() => {
+        fetch('fakedata.json')
+            .then(res => res.json())
+            .then(data => setActivities(data))
+    }, [])
 
-//     return (
-//         <div className="mt-20 container mx-auto flex flex-row justify-center manrope-font">
-//             <div className="w-4/6">
-//                 <div className="grid grid-cols-3 gap-y-10">
-//                     {/*  */}
-//                     {
-//                         activities.map(activity => <Activity
-//                             key={activity.id}
-//                             activity={activity}
-//                         ></Activity>)
-//                     }
-//                 </div>
-//             </div>
+    return (
+        <div className="mt-20 container mx-auto flex flex-row justify-center manrope-font">
+            <div className="w-4/6">
+                <div className="grid grid-cols-3 gap-y-10">
+                    {/*  */}
+                    {
+                        activities.map(activity => <Activity
+                            key={activity.id}
+                            activity={activity}
+                        ></Activity>)
+                    }
+                </div>
+            </div>
 
-//             <div className="w-2/6">
-//                 <div className="grid gap-y-10">
-//                     <div className="w-96 h-72 bg-gray-300 rounded overflow-hidden shadow-lg">
-//                         <div className="flex flex-col items-center">
-//                             <h2 className="mt-4 mb-2 text-2xl font-semibold text-center"> </h2>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// };
+            {/* <div className="w-2/6">
+                <div className="grid gap-y-10">
+                    <div className="w-96 h-72 bg-gray-300 rounded overflow-hidden shadow-lg">
+                        <div className="flex flex-col items-center">
+                            <h2 className="mt-4 mb-2 text-2xl font-semibold text-center"> </h2>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+        </div>
+    );
+};
 
-// export default Activities;
+export default Activities;
--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,19 +1,19 @@
-// import React from 'react';
+import React from 'react';
 
-// const Activity = (props) => {
-//     const { name, time, picture } = props.activity;
-//     return (
-//         <div className="w-4/5 h-80 bg-gray-200 border border-gray-600 rounded overflow-hidden shadow-lg">
-//             <img className="w-72 h-36" src={picture} alt="Sunset in the mountains" />
-//             <div className="flex flex-col justify-center items-center text-black">
-//                 <h2 className="text-2xl font-medium mt-4 mb-2">{name}</h2>
-//                 <h6 className='mb-6' >Time: {time}</h6>
-//                 <button
-//                     className="bg-violet-800 font-semibold rounded px-16 py-2 mb-4 text-center text-white">Select</button>
-//             </div>
-//         </div>
+const Activity = (props) => {
+    const { name, time, picture } = props.activity;
+    return (
+        <div className="w-4/5 h-80 bg-gray-200 border border-gray-600 rounded overflow-hidden shadow-lg">
+            <img className="w-72 h-36" src={picture} alt="Sunset in the mountains" />
+            <div className="flex flex-col justify-center items-center text-black">
+                <h2 className="text-2xl font-medium mt-4 mb-2">{name}</h2>
+                <h6 className='mb-6' >Time: {time}</h6>
+                <button
+                    className="bg-violet-800 font-semibold rounded px-16 py-2 mb-4 text-center text-white">Select</button>
+            </div>
+        </div>
 
-//     );
-// };
+    );
+};
 
-// export default Activity;
+export default Activity;
