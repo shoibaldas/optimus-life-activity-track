934cac00a7476309a292a4ee5db2e4fd4c4e61f0
shoibaldas <shoibalshantu@gmail.com>
2022-09-28 22:50:06 +0600

    Navbar initialized

---
+++ src/components/Header/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.css'
+const navbar = () => {
+    return (
+        <nav>
+            <label className="nav-title">Optimus Life</label>
+            <ul>
+                <li><a href=""></a>Home</li>
+                <li><a href=""></a>About</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default navbar;
